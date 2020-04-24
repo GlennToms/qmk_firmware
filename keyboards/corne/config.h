51ea02b2af7b562e7595232104afdb107a8f226c
GlennToms <Glenn@glenntoms.gg>
2020-04-24 15:44:16 +0000

    left side working

--- keyboards/corne/config.h
+++ keyboards/corne/config.h
@@ -45,7 +45,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
  // https://deskthority.net/wiki/Arduino_Pro_Micro#Pinout
 #define MATRIX_ROW_PINS { D4, C6, D7, E6 }
-#define MATRIX_COL_PINS { B4, B5, F4, F5, F6, F7}
+#define MATRIX_COL_PINS {F4, F5, F6, F7, B5, B4}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
