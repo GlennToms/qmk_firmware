37a129b34bfafd907c97b0e85ec1817056c0d341
GlennToms <Glenn@glenntoms.gg>
2020-04-24 14:56:04 +0000

    It compiles

--- keyboards/corne/config.h
+++ keyboards/corne/config.h
@@ -42,8 +42,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D4, D5, D6, D7 }
-#define MATRIX_COL_PINS { D8, D9, A3, A2, A1 }
+
+ // https://deskthority.net/wiki/Arduino_Pro_Micro#Pinout
+#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
+#define MATRIX_COL_PINS { B4, B5, F4, F5, F6, F7}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
